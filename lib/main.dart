34ed6b1a0cfd9c023cbb0d686c832342008e754d
EveSquare <jonjonjonjon152@gmail.com>
2022-05-12 11:52:57 +0900

    [update] header更新, Body切り分け

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -45,79 +46,107 @@ class _MyWidgetState extends State<MyWidget> {
     final ButtonStyle style =
         TextButton.styleFrom(primary: Theme.of(context).colorScheme.onPrimary);
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Flexible(
-                child: Image.asset(
-                  "assets/images/sun.gif",
-                  height: 30,
-                ),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Flexible(
+              child: Image.asset(
+                "assets/images/sun.gif",
+                height: 30,
               ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        child: const Text(
-                          "やや曇り",
-                          style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 13,
-                          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                width: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "やや曇り",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 13,
                         ),
                       ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        child: const Text(
-                          '最高:$high° 最低:$low°',
-                          style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 13,
-                          ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        '最高:$high° 最低:$low°',
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 13,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-          actions: <Widget>[
-            if (mute == true)
-              IconButton(
-                icon: const Icon(Icons.volume_mute),
-                tooltip: 'mute',
-                color: Colors.black,
-                onPressed: () {
-                  toggleMute();
-                },
-              ),
-            if (mute == false)
-              IconButton(
-                icon: const Icon(Icons.volume_up),
-                tooltip: 'volume',
-                color: Colors.black,
-                onPressed: () {
-                  toggleMute();
-                },
-              ),
-            IconButton(
-              icon: const Icon(Icons.settings),
-              tooltip: 'settings',
-              color: Colors.black,
-              onPressed: () {},
             ),
           ],
-          elevation: 0,
-          backgroundColor: Colors.white,
         ),
-        body: const Scaffold(
-          backgroundColor: Color.fromRGBO(222, 231, 244, 100),
-        ));
+        actions: <Widget>[
+          if (mute == true)
+            IconButton(
+              icon: const Icon(Icons.volume_mute),
+              tooltip: 'mute',
+              color: Colors.black,
+              onPressed: () {
+                toggleMute();
+              },
+            ),
+          if (mute == false)
+            IconButton(
+              icon: const Icon(Icons.volume_up),
+              tooltip: 'volume',
+              color: Colors.black,
+              onPressed: () {
+                toggleMute();
+              },
+            ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'settings',
+            color: Colors.black,
+            onPressed: () {},
+          ),
+        ],
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Center _buildBody() {
+    String getNow() {
+      DateTime now = DateTime.now();
+      DateFormat Outformat = DateFormat("yyyy年MM月dd日");
+      String date = Outformat.format(now);
+      return date;
+    }
+
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            getNow(),
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            "文化の日",
+            style: TextStyle(
+              color: Colors.red[400],
+              fontSize: 22,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
